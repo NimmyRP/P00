76450c46a79e0c97cd06d6c55881695fed0d1fad
nimmy_varghese <nimmy_varghese@SFT-OIS28490.rp.edu.sg>
2022-05-24 15:08:40 +0800

    First push to P00 repository in GitHub

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+//Class container for a cohesive set of tests
+
+public class CalculatorTest {
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	//Name of unit test
+	public void test() {
+		
+		// Arrange
+		int n1=10;
+		int n2=20;
+		Calculator cal=new Calculator();
+		
+		//Act
+		int actual =cal.sum(n1,n2);
+		
+		// Assert
+				
+		assertEquals(30,actual);	
+		
+		
+	}
+
+}
