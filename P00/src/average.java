8615439dc67796717b88963f6b5576adec813953
nimmy_varghese <nimmy_varghese@SFT-OIS28490.rp.edu.sg>
2022-05-26 15:06:58 +0800

    First push to P00 repositiry in GitHub

---
+++ P00/src/average.java
@@ -0,0 +1,9 @@
+
+public class average {
+	
+	public int aveargeoffive(int n1,int n2,int n3, int n4,int n5)
+	{
+	return (n1+n2+n3+n4+n5)/5;
+	}
+
+}
---
+++ P00/test/averageTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class averageTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	//Name of unit test
+	public void testAverage()
+	
+	{
+		
+		// Arrange
+		int n1=10;
+		int n2=20;
+		int n3=10;
+		int n4=20;
+		int n5=30;
+		average Avg=new average();
+		
+		//Act
+		int actual =Avg.aveargeoffive(n1,n2,n3,n4,n5);
+		
+		// Assert
+				
+		assertEquals(18,actual);
+
+   }
+}
